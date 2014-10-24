912337d5dc7cc214d0a315a4121c24c4586b1bbc
Egon Willighagen <egon.willighagen@gmail.com>
2014-10-24 18:20:06 +0200

    Import Map

--- WpRDFFunctionLibrary.java
+++ WpRDFFunctionLibrary.java
@@ -18,6 +18,7 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
